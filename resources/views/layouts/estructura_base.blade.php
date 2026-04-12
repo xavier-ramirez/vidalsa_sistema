7293bfcf00d0e84902f38a5c54c661e7b9e0bee9
Usuario <fsanchez@cvidalsa27.com>
2026-04-12 00:20:11 -0400

    fix(cache): bump equipos_index.js version constraint to v22.0
    - forces browsers to ditch the dirty cached javascript containing the 'null' string bug
    - ensures the newly built strict validation for 'Desanclar' activates instantly on production

--- resources/views/layouts/estructura_base.blade.php
+++ resources/views/layouts/estructura_base.blade.php
@@ -462,7 +462,7 @@
     {{-- from navegacion.js when switching between pages without reload --}}
     <script src="{{ asset('js/maquinaria/menu.js') }}?v=5.2"></script>
     <script src="{{ asset('js/maquinaria/catalogo_create.js') }}?v=12.2"></script>
-    <script src="{{ asset('js/maquinaria/equipos_index.js') }}?v=21.2"></script>
+    <script src="{{ asset('js/maquinaria/equipos_index.js') }}?v=22.0"></script>
     <script src="{{ asset('js/maquinaria/catalogo_index.js') }}?v=4.0"></script>
     <script src="{{ asset('js/maquinaria/movilizaciones_index.js') }}?v=9.1"></script>
     <script src="{{ asset('js/maquinaria/usuarios_index.js') }}?v=10.2"></script>
